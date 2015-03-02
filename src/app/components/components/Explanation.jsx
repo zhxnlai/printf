e67f696eaabedf726a58d17c03e62bf467f16f99
Zhixuan Lai <zhxnlai@gmail.com>
2015-03-02 02:38:00 -0800

    adding explanation content and implementation

--- src/app/components/components/Explanation.jsx
+++ src/app/components/components/Explanation.jsx
@@ -5,11 +5,31 @@ var assign = require('object-assign');
 var EditorStore = require('../../stores/EditorStore.js');
 var EditorActionCreators = require('../../actions/EditorActionCreators.js');
 
+var Format = require('./Explanations/Format.jsx');
+var Parameter = require('./Explanations/Parameter.jsx');
+
 function getStateFromStores() {
   return {
     highlightedNode : EditorStore.getHighlightedNode(),
   };
 }
+
+var displayNameToId = {
+  "format": "format-placeholders",
+  "(parameter)?": "parameter",
+  "(flags)?": "flags",
+  "(width)?": "width",
+  "(precision)?": "precision",
+  "(length)?": "length",
+  "specifier": "specifier",
+
+  "parameter": "parameter",
+  "flags": "flags",
+  "width": "width",
+  "precision": "precision",
+  "length": "length",
+
+};
 
 var Explanation = React.createClass({
   mixins: [Classable],
@@ -33,23 +53,56 @@ var Explanation = React.createClass({
     this.setState(getStateFromStores());
   },
 
-  propTypes: {
-    // value: React.PropTypes.string.isRequired,
+  showNode: function (oNode) {
+    var nLeft = 0, nTop = 0;
+    for (var oItNode = oNode; oItNode; nLeft += oItNode.offsetLeft, nTop += oItNode.offsetTop, oItNode = oItNode.offsetParent);
+    var node = this.getDOMNode();
+    node.scrollTop = nTop;
+    node.scrollLeft = nLeft;
+
+    // TODO: highlight
   },
 
-  onEditorTextChange: function(e) {
-    EditorActionCreators.textChange(e.target.value);
+  showBookmark: function(sBookmark, bUseHash) {
+    if (arguments.length === 1 || bUseHash) {
+        location.hash = sBookmark;
+        return;
+    }
+    // var oBookmark = this.getDOMNode().querySelector(sBookmark);
+    var oBookmark = document.getElementById(sBookmark);
+    if (oBookmark) {
+        this.showNode(oBookmark);
+    }
+  },
+
+  componentDidUpdate: function() {
+    if (this.state.highlightedNode) {
+      var displayString = this.state.highlightedNode.displayString;
+      var elementId = displayNameToId[displayString] ? displayNameToId[displayString]
+                                                     : displayString;
+      this.showBookmark(elementId, false);
+    }
   },
 
   render: function() {
     var classes = this.getClasses('explanation', {
     });
 
-    console.log(this.state.highlightedNode);
+    // console.log(this.state.highlightedNode);
+    var subtitle = "";
+    if (this.state.highlightedNode) {
+      var displayString = this.state.highlightedNode.displayString;
+      subtitle = "Showing explanation for: "+displayString;
+    }
 
     return (
       <div className={classes}>
-        <h1>Explanation</h1>
+        <div className="explanation-body">
+          <h1>Printf Format String</h1>
+          <h2>{subtitle}</h2>
+          <Format/>
+          {/*<Parameter/>*/}
+        </div>
       </div>
       );
     }
