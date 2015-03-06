4c656f99662bfb89bd6dadb7ef6676d5e6841a59
Zhixuan Lai <zhxnlai@gmail.com>
2015-03-06 03:12:17 -0800

    adding deploy to gh pages

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -21,13 +21,13 @@
   Router
   // Runs the router, similiar to the Router.run method. You can think of it as an
   // initializer/constructor method.
-  // .create({
-  //   routes: AppRoutes,
-  //   // scrollBehavior: Router.ScrollToTopBehavior
-  // })
+  .create({
+    routes: AppRoutes,
+    // scrollBehavior: Router.ScrollToTopBehavior
+  })
   // This is our callback function, whenever the url changes it will be called again.
   // Handler: The ReactComponent class that will be rendered
-  .run(AppRoutes, Router.HistoryLocation, function (Handler) {
+  .run(/*AppRoutes, Router.HistoryLocation,*/ function (Handler) {
     React.render(<Handler/>, document.body);
   });
 
